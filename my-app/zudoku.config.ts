018a008d3f59f003bf942ebc870ec86e38fc3633
Ptyg <nawkking@gmail.com>
2025-03-15 17:12:26 +0200

    feat: update docs

--- my-app/zudoku.config.ts
+++ my-app/zudoku.config.ts
@@ -1,13 +1,21 @@
 import type { ZudokuConfig } from "zudoku";
 
 const config: ZudokuConfig = {
-  topNavigation: [],
+  topNavigation: [
+    { id: "about", label: "About" },
+    { id: "authorization", label: "Authorization" },
+    { id: "installation", label: "Installation" },
+    { id: "api", label: "API" },
+  ],
   sidebar: {},
   redirects: [{ from: "/", to: "/api/clients" }],
   apis: {
     type: "file",
     input: "./apis/openapi.yaml",
     navigationId: "api",
+  },
+  docs: {
+    files: "/pages/**/*.{md,mdx}",
   },
 };
 
